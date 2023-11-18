4e8b8ef714277a181862b578c7f4b9d7496a6880
VaniVl <moskvitin.228@gmail.com>
2023-11-18 14:13:35 +0300

    heyGen comments added

--- python/keyGen.py
+++ python/keyGen.py
@@ -2,9 +2,9 @@ from web3 import Web3
 import pandas as pd
 
 connection = Web3()
-def gen_keys(name):
+def gen_keys(name): #принимает значение имени профиля и возвращает публичный и приватный ключ
     list_of_dicts = []
-    account = connection.eth.account.create()
+    account = connection.eth.account.create() # генерируем новые ключи и после присваиваем их переменным
     address = account.address
     private_key = account.key.hex()
 
@@ -18,4 +18,4 @@ def gen_keys(name):
     dictionary['private_key'] = private_key
 
     list_of_dicts.append(dictionary)
-    return list_of_dicts
+    return list_of_dicts 
